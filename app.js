10607bbc64fe65e08d7d2ec276e60403125c129f
AnnYablonskaya <yaanna1910@gmail.com>
2022-05-30 21:21:38 +0300

    refactor: use BEM according to rule 2.1 in html-and-css-exteded.md

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.classList.add('li-list');
+    listItem.classList.add('task__list');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -46,10 +46,10 @@ var createNewTaskElement=function(taskString){
     editInput.classList.add('input-todo');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button_edit";
     editButton.classList.add('button');
 
-    deleteButton.className="delete";
+    deleteButton.className="button_delete";
     deleteButton.classList.add('button');
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.classList.add('images');
@@ -92,7 +92,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".button_edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -164,8 +164,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".button_edit");
+    var deleteButton=taskListItem.querySelector(".button_delete");
 
 
     //Bind editTask to edit button.
